044fcc602382a033304472341e6d3e945444433d
IbnJammaz <50358732+ibnJammaz@users.noreply.github.com>
2023-12-28 09:09:36 +0300

    Several updates

--- vt-check.py
+++ vt-check.py
@@ -4,37 +4,21 @@ __description__ = "check IoCs [IPs/hashs/URLs] in VirusTotal using virustotal AP
 __author__ = "Omar Aljammaz"
 __date__ = "25/12/2023"
 
+
 import argparse
 import sys
 import requests
-import json
 import time
 import ipaddress
 
 API_KEY = "<add_your_api_here>"
-headers = {
-    'Content-Type': 'application/json',
-    'x-apikey': API_KEY
-    }
-logo = r'''
-   o     o                              __o__                                                                      
- _<|>_  <|>                               |                                                                        
-        / >                              / \                                                                       
-   o    \o__ __o     \o__ __o            \o/      o__ __o/  \o__ __o__ __o   \o__ __o__ __o      o__ __o/  _\o____ 
-  <|>    |     v\     |     |>            |      /v     |    |     |     |>   |     |     |>    /v     |       /   
-  / \   / \     <\   / \   / \           < >    />     / \  / \   / \   / \  / \   / \   / \   />     / \    o/    
-  \o/   \o/      /   \o/   \o/   \        |     \      \o/  \o/   \o/   \o/  \o/   \o/   \o/   \      \o/   /v     
-   |     |      o     |     |     o       o      o      |    |     |     |    |     |     |     o      |   />      
-  / \   / \  __/>    / \   / \    <\__ __/>      <\__  / \  / \   / \   / \  / \   / \   / \    <\__  / \  \>__o__ 
-                                                                                                                \  
-'''
-ip_check = False
-hash_check = False
-url_check = False
+headers = {"Content-Type": "application/json", "x-apikey": API_KEY}
+
 url = ""
 vt_hash_link = "https://www.virustotal.com/api/v3/files/"
 vt_ip_link = "https://www.virustotal.com/api/v3/ip_addresses/"
 vt_url_link = "https://www.virustotal.com/api/v3/domains/"
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -52,31 +36,27 @@ def parse_arguments():
         help="path to .txt file hashs list",
         required=True,
     )
-    
-    
-    optional.add_argument("-h", "--help", action="help", help="show this help message and exit")
+
+    optional.add_argument(
+        "-h", "--help", action="help", help="show this help message and exit"
+    )
     return parser.parse_args()
+
 
 def identify_ioc(IoC):
     try:
         ipaddress.ip_address(IoC)
-        ip_check = True
         return "is_ip"
-    except Exception:
-        ip_check = False
-    try:
-        if len(IoC) == 32 or len(IoC) == 64 or len(IoC) == 40:
-            if "." in IoC:
-                return "is_url"
-            hash_check = True
-            return "is_hash"
-        else:
-            hash_check = False
-            url_check = True
+    except ValueError:
+        pass
+    if len(IoC) == 32 or len(IoC) == 64 or len(IoC) == 40:
+        if "." in IoC:
             return "is_url"
-    except Exception:
-        print (f"error in identifying hash: {Exception}")
-
+        return "is_hash"
+    elif IoC.startswith("http"):
+        return "is_url"
+    else:
+        print(f"error in identifying IOC: {IoC}")
 
 
 def VT_ckeck(IoC):
@@ -85,7 +65,7 @@ def VT_ckeck(IoC):
         # print(ioc_type)
         if ioc_type == "is_ip":
             url = vt_ip_link
-        elif(ioc_type == "is_hash"):
+        elif ioc_type == "is_hash":
             url = vt_hash_link
         else:
             url = vt_url_link
@@ -102,13 +82,15 @@ def VT_ckeck(IoC):
                 print(f"{IoC[i]} NOT found")
                 continue
         else:
-            print(f"{IoC[i]} flaged as malicious in: {decodedResponse["data"]["attributes"]["last_analysis_stats"]["malicious"]} Vendors")
+            print(
+                f"{IoC[i]} flaged as malicious in: {decodedResponse["data"]["attributes"]["last_analysis_stats"]["malicious"]} Vendors"
+            )
         time.sleep(15)
 
 
 def main():
     args = parse_arguments()
-    
+
     try:
         with open(args.list) as f:
             IoC = f.read().splitlines()
@@ -119,13 +101,12 @@ def main():
     VT_ckeck(IoC)
 
 
-
-
 if __name__ == "__main__":
-    print(logo)
     try:
         if API_KEY == "<add_your_api_here>":
-            print(r'''add you VT API in line 14 in .py file: API_KEY = "<add_yout_api_here>"''')
+            print(
+                r'''add you VT API in line 14 in .py file: API_KEY = "<add_yout_api_here>"'''
+            )
         else:
             main()
 
